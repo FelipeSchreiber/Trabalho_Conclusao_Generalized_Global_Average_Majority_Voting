41617397bbc4cfe44c0372201e8ac6072790123e
FelipeSchreiber <schreiber.felipe@poli.ufrj.br>
2021-11-12 15:27:09 -0300

    Add files via upload

--- distributed_gam/distributed_GAM.scala
+++ distributed_gam/distributed_GAM.scala
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.graphx._
 import org.apache.spark._
+import java.io._
 
 //Para compilar, faça sbt assembly. Para executar, $SPARKHOME/spark-submit --class "Nome_da_classe" caminho_ate_o_jar
 object SimpleGraphApp {
@@ -20,30 +21,42 @@ object SimpleGraphApp {
     val filePath = "/home/felipe/UFRJ/TCC/spark-3.1.2-bin-hadoop3.2/data/graphx/"
     // Load some data into RDDs
     val graph = GraphLoader.edgeListFile(sc,filePath+"/ppg_net.txt")
+	def writeToFile(id:Long,label:Long,file:PrintWriter)
+	{
+		file.write(String.valueOf(id))
+		var tempStr = ""
+		tempStr = "," + String.valueOf(label)
+        file.write(tempStr)
+		file.write("\n")
+	}
     
     //LPA algorithm
-    val lpaGraph = graph.mapVertices { case (vid, _) => vid%2 }
+	//val r = new scala.util.Random
+	//r.nextInt(1).toLong
+    var lpaGraph = graph.mapVertices { case (vid, _) =>  vid%2}
     val V = graph.numVertices
     
     type Label = VertexId
 	val initialMessage = Map[Label, Long]()
-	var g:Double = 0.5
+	val g = sc.doubleAccumulator("my accum")
+	lpaGraph.vertices.foreach{a => g.add(a._2.toDouble/V)}
+	println("Initial G value: "+g.value.toString)
 	
 	def sendMsg(e: EdgeTriplet[Label, Int]): Iterator[(VertexId,Map[Label, Long])] =
 		Iterator((e.srcId, Map(e.dstAttr -> 1L)), 
 		(e.dstId,Map(e.srcAttr -> 1L)))
-		
-	def vprog(vid: VertexId, attr: Long, message: Map[Label, Long]): VertexId = 
+
+	def vprog(vid: VertexId, attr: Long, message: Map[Label, Long]): Long = 
 	{
-		//var localMax = message.maxBy(_._2)._1
-		var label = 0
+		var label: Long = 0
 		if (message.isEmpty) 
 			return attr 
 		else 
 		{
-			var localAvg:Long = 0
-			message.foreach(x => localAvg += x._2.toLong)
-			if (localAvg > g*V)
+			var localAvg:Double = message.getOrElse(1,0L).toDouble
+			localAvg /= (message.getOrElse(0,0L) + message.getOrElse(1,0L)).toDouble
+			//println("Local avg "+vid.toString+" "+localAvg.toString) 
+			if (localAvg > g.value)
 				label = 1
 			else
 				label = 0 
@@ -60,15 +73,20 @@ object SimpleGraphApp {
 			i -> (count1Val + count2Val)
 		}.toMap
 	}
+
 	var i = 0
 	while(i < maxIter)
 	{
-		lpaGraph.pregel(initialMessage, step)(vprog, sendMsg, mergeMsg)
+		lpaGraph = lpaGraph.pregel(initialMessage, step)(vprog, sendMsg, mergeMsg)
 		i += step
-		g=0
-		lpaGraph.vertices.foreach(a => g += a._2)
-		g /= V
+		g.reset
+		println("G value: "+g.value.toString)
+		lpaGraph.vertices.foreach(a => g.add(a._2.toDouble/V))
+		println("G value: "+g.value.toString)
+		println(i)
 	}
-	lpaGraph.vertices.take(10).map(println)
+	val writer = new PrintWriter(new File("results.txt"))
+	lpaGraph.vertices.collect.map{case(id,it) => writeToFile(id,it,writer)}
+	writer.close()
   }
 }
